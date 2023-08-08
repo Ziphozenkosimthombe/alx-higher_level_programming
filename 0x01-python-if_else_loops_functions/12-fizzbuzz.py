8a7d5860c0249cde228e888be6aacf773eaf47ff
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-08 08:54:36 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,6 +5,8 @@ def fizzbuzz():
             print('FizzBuzz', end="")
         elif number % 3 == 0:
             print('Fizz', end="")
+        elif number % 5 == 0:
+            print('Buzz', end="")
         else:
             print('{}'.format(number), end="")
 
