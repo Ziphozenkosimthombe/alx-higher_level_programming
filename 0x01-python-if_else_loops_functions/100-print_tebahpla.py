96e7b5f62fee44bd6c490f3b3170a1894233630f
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-08 09:37:21 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 j = 0
-for c in range(ord('z'),ord('a') -1, -1):
+for c in range(ord('z') ,ord('a')-1, -1):
     print('{}'.format(chr(c - j)), end="")
     j = 32 if j == 0 else 0
