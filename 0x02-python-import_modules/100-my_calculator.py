3abc191ac0ac89ccf5d86ad7ff4583979908acb8
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-10 13:05:03 -0700

    python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
