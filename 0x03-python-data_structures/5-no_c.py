9f3fd702cb6657401b20a067215153bfc9d38b99
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-13 12:33:06 -0700

    python-data_structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""removes all charectors c nad C from a string"""
+
+
+def no_c(my_string):
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return("".join(copy))
