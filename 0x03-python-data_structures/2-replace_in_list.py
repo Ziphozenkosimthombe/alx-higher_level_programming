c768cb6a101340eaffb2024afca03264fe74b8ec
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-13 06:37:24 -0700

    python-data_structures

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""replaces an element of a list at a specific position"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
