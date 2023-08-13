2d061c87b5ff207af0746cbd737cf1a5a05fc6c9
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-13 12:21:27 -0700

    python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-""" replaces an element in a list at a specific position without modifying the original list
+""" replaces an element in a list at a specific position
 """
+
 
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
