2fe49e3b5837dba32f0f467d6359f296957095f4
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-14 02:37:33 -0700

    python-data_structures

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""returns a tuple with the length of a string and its first charecter"""
+
+
+def multiple_returns(sentence):
+    if sentence == "":
+        return None
+    return (len(sentence), sentence[0])
