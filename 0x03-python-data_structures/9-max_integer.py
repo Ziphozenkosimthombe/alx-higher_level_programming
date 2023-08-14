707bc46a2db23fdfb4e4e99ffe9cc8c6b7c65c3f
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-14 02:58:39 -0700

    python-data_structures

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""function that finds the biggest integer of list"""
+
+
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return None
+    greater = 0
+    for i in range(len(my_list)):
+        if my_list[i] > greater:
+            greater = my_list[i]
+    return greater
