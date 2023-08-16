5105f00042c30c55d18585aa681072f6de102d02
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-16 07:03:18 -0700

    python-more_data_structures

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""returns a set of common elements in two sets"""
+
+def common_elements(set_1, set_2):
+    result =[]
+
+    for i in set_1:
+        if i in set_2:
+            result.append(i)
+    return result
