f0b23007b1df217800fc3681fe23a9af4be46688
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-16 11:01:46 -0700

    python-more_data_structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""returns the weighted average of all integers tuple (<score>, <weight>)"""
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+
+    number = 0
+    weight = 0
+
+    for i in my_list:
+        number += i[0] * i[1]
+        weight += i[1]
+
+    return (number/weight)
