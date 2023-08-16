87d402b07f76d05fbb47d10208e114daeb12f10d
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-16 11:36:05 -0700

    python-more_data_structures

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""deletes keys with a specific value in a dictionary"""
+
+
+def complex_delete(a_dictionary, value):
+    listForKeys = list(a_dictionary.keys())
+
+
+    for dictionaryValue in listForKeys:
+        if value == a_dictionary.get(dictionaryValue):
+            del a_dictionary[dictionaryValue]
+    return a_dictionary
