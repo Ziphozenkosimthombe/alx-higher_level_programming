7af6e4baa67f80f7081e8e0c1793ada175fc8ac7
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-16 13:12:31 -0700

    python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,7 +16,6 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     listForKeys = list(rom.keys())
 
