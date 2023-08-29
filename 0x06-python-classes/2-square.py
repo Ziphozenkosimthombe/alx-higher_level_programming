c8156a777428597ce64f4eaa0562c28ec28e9b67
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-29 09:06:56 -0700

    python-classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""define the class square"""
+
+
+class Square:
+    """reprenting the square"""
+    def __init__(self, size=0):
+        """initializing the new square.
+
+        Args:
+            size(int): the size of the new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
