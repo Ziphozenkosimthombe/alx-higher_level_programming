a5dc8bd3099ce1b32707932101b50d3112f5497f
Ziphozenkos <ziphomthombe478@gmail.com>
2023-08-29 08:53:09 -0700

    python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+"""class square defining"""
+
 
 class Square:
+    """represent the saqure"""
 
     def __init__(self, size):
+        """initializing new square.
+        Args:
+            size(int): the size of the new square"""
 
         self.__size = size
