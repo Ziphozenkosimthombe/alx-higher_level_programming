81e41edf465d1eb49ab85225be4ac5f2354d6ca5
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-05 09:07:29 -0700

    python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""delcare the class of LockedClass"""
+
+
+class LockedClass:
+
+    """define a class attribute __slots__ with only one element 'frist_name'"""
+
+    __slots__ = ["first_name"]
