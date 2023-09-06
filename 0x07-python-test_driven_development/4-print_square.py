f109ee06156edbc0f20d83080954301432acf804
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-06 02:25:01 -0700

    python-test_driven_development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,6 +15,5 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     for k in range(size):
-       [print('#', end='')
-               for a in range(size)]
-       print('')
+        [print('#', end='')for a in range(size)]
+        print('')
