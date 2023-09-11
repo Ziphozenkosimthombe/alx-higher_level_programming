5acee4c89ec4409325a19fe2590737beaf1f6db4
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-11 08:25:07 -0700

    python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 """declare the class inherits"""
 
 
 class MyList(list):
-    """the with the inherits of the list"""
+    """the class with the inherits of the list"""
 
     def print_sorted(self):
         """function will print the list, sorted"""
