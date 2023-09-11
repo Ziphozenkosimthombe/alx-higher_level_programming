9a32e947e8af05c63b38b48a2b5633e74a5161b5
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-11 12:59:45 -0700

    python-inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""declare the inhrits the Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+
+class Square(Rectangle):
+    """intitialazing the square"""
+
+    def __init__(self, size):
+        """initialazing the new square"""
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
