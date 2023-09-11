90f7b35035e637fe3641c05f1c82dfdbdbb74d01
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-11 11:31:13 -0700

    python-inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""declare the inherits from Rectangel"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator('size',size)
