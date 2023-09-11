1d075a85208392203f6b75044539fa68e90581cc
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-11 09:48:04 -0700

    python-inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""declare the class"""
+
+
+class BaseGeometry:
+    """the class of the BaseGeometry"""
+    def area(self):
+        """it raise an exception withe the message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """the validates of the value"""
+        if type(value) is not  int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
