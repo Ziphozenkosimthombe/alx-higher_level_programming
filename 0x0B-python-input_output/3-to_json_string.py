47a5a5850c738601759c4ef403f7aabbd81d9a81
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-12 07:46:25 -0700

    python-input_output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""importing the mudule of the json"""
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an,
+    object
+    """
+    return json.dumps(my_obj)
