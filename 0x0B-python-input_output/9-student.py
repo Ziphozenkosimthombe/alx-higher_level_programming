dadbe009da367899ab7d71ca5398a3bf2eb66c7e
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-12 09:44:02 -0700

    python-input_output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defining the class"""
+
+
+class Student:
+    """initialazing the class of student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
