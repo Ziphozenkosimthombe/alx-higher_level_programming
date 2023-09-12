7a3aa3063439d408a07e10198e9aa93716766093
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-12 09:21:56 -0700

    python-input_output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module that adds all arguments to a python list"""
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        item = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item = []
+        item.extend(sys.argv[1:])
+        save_to_json_file(item, 'add_item.json')
