71ca807a79b263dd68307d3ecc264f829be1cdf4
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-12 09:59:33 -0700

    python-input_output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defining the class"""
+
+
+class Student:
+    """initialazing the class of student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and 
+                all(type(le) == str for le in attrs)):
+            return {k: getattr(self, k) for k  in attrs if hasattr(self, k)}
+        return self.__dict__
