984331451827278bffac890db4e3d22174214b28
Ziphozenkos <ziphomthombe478@gmail.com>
2023-09-12 10:34:26 -0700

    python-input_output

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+ #!/usr/bin/python3
+"""defining the function of Pscal's triangle of n"""
+
+
+def pascal_triangle(n):
+    """Check if n is a valid input"""
+    if n <= 0:
+        return []
+    """Initialize the triangle with the first row"""
+    triangle = [[1]]
+    """ Loop from the second row to the n row"""
+    for a in range(1, n):
+        """Initialize the current row with the first element as 1"""
+        row = [1]
+        """ Loop from the second element to the second last element"""
+        for b in range(1, a):
+            """Calculate the current element as the sum of the two elements above it"""
+            row.append(triangle[a-1][b-1] + triangle[a-1][b])
+        row.append(1)
+        triangle.append(row)
+    return triangle
