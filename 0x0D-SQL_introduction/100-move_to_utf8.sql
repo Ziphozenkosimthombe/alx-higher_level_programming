30debf9f1f9d85412f0a06d746ed7fe1a5951593
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 09:51:59 -0700

    SQL_introduction

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- need to convert all of the following to UTF8:
+-- Database hbtn_0c_0
+-- Table first_table
+-- Field name in first_table
+
+USE `hbtn_0c_0`
+ALTER `first_table`
+CONVERT TO CHARACTER SET UTF8 COLLATE utf8mb4_unicode_ci;
