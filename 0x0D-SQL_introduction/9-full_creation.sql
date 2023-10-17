8778d94b41ff5823a4e139748c4c91adfd2dc930
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 08:37:47 -0700

    SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,15 +1,15 @@
---  script that creates a table second_table in the database hbtn_0c_0
--- add multiples rows.
+-- script that creates a table second_table in the database hbtn_0c_0
+
 CREATE TABLE IF NOT EXISTS second_table
 (
-	id INT,
+	id INT AUTO_INCREMENT,
 	name VARCHAR(256),
-	score INT,
+	score INT
 );
 
-INSERT INTO second_table(id, name, score)
+INSERT INTO second_table(name, score)
 VALUES
-	(1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(4, 'George', 8);
+	('John', 10),
+	('Alex', 3),
+	('Bob', 14),
+	('George', 8);
