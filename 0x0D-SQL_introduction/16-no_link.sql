37d5c4c20122f81041a4e13251db2590c3d8947c
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 09:39:55 -0700

    SQL_introduction

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,10 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0
+-- Don’t list rows without a name value
+-- Results should display the score and the name 
+-- Records should be listed by descending score
+-- database name will be passed as an argument to the mysql command
+
+SELECT score, name
+FROM second_table
+ORDER BY score, name
+DESC
