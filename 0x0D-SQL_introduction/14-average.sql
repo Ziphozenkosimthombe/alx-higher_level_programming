4b6161b4715036e0579a6f541013a22ca63f0beb
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 09:16:56 -0700

    SQL_introduction

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,8 @@
+-- script that computes the score average of all records in the table second_table of the database hbtn_0c_0
+-- The result column name should be average.
+-- database name will be passed as an argument of the mysql command
+
+SELECT score
+COUNT (score) AS average
+FROM second_table
+GROUP BY score;
