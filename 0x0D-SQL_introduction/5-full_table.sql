d7721e746263a404576bb94bb3c7d13097b5e30f
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 07:07:31 -0700

    SQL_introduction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
--- script that prints the full description of the table first_table.
--- database name will be passed as an argument of the mysql command.
--- not allowed to use the DESCRIBE or EXPLAIN statement.
+-- show the full description of the table first_table
 
-SHOW CREATE TABLEfirst_table
+SHOW CREATE TABLE first_table
