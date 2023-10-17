b35fc98cb6a1e4a8eb87aea86c811a9a6866337d
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 06:52:33 -0700

    SQL_introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -6,7 +6,7 @@
 -- If the table first_table already exists, your script should not fail
 -- not allowed to use the SELECT or SHOW statements
 
-CREATE TABLE IF EXISTS first_table
+CREATE TABLE IF NOT EXISTS first_table
 (
 	id INT,
 	name VARCHAR(256)
