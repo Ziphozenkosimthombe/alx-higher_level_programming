90177c3dd97eb4fd5fe1c6c0b360b2042d82d182
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 09:09:29 -0700

    SQL_introduction

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,6 +1,6 @@
 -- script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
 -- The database name will be passed as an argument of the mysql command
 
-DROP second_table
+DROP FROM second_table
 where score <= 5
 DESC
