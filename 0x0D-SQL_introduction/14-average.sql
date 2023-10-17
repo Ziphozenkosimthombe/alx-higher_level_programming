9dde23531ed6b468eba1dc602c4f175523928343
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-17 09:20:49 -0700

    SQL_introduction

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -2,7 +2,6 @@
 -- The result column name should be average.
 -- database name will be passed as an argument of the mysql command
 
-SELECT score
-COUNT(score) AS average
+SELECT COUNT(score) AS average
 FROM second_table
 GROUP BY score;
