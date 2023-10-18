d3f614b6f28f5965e5d64ac0d2dad7fa26a26f55
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-18 06:35:59 -0700

    SQL_more_queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -11,7 +11,7 @@ CREATE DATABASE IF NOT NULL hbtn_0d_usa;
 CREATE TABLE IF NOT NULL hbtn_0d_usa.cities
 (
 	id INT AUTO_INCREMENT NOT NULL,
-	state INT NOT NULL,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY(state_id) REFERENCES states(id)
