14e9b47ac015cefd650fb197deeb7797f3e83895
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-18 05:48:10 -0700

    SQL_more_queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,9 +5,9 @@
 
 CREATE USER
 	IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd'
+	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	ON *.*
 	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd'
+	IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
