0f5a0bfeb57c3c1c5996ede8a78dbda39783a934
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-13 10:52:46 -0800

    javascript-warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 const num = Number(process.argv[2]);
-const fact = number => (number === 0 || isNaN(number)) ? 1 : number * fact(number - 1);
 if (num >= 0 || isNaN(num)) { console.log(fact(num)); }
+function fact (number) {
+  if (number === 0 || isNaN(number)) return 1;
+  else return number * fact(number - 1);
+}
