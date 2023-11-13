b4e5a16eacb061444ceed46e75d4d43e2f45db97
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-13 08:45:29 -0800

    javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,4 +2,4 @@
 /* script that prints 3 lines: (like 1-multi_languages.js)
   but by using an array of string and a loop */
 const arrLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-arrLang.map(item => console.log(item));
+for (let i = 0; i < arrLang.length; i++) { console.log(arrLang[i]); }
