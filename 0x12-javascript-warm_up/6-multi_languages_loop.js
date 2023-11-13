5cbe8c61fdd25706a1dd7cb606ed3a032608f0b4
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-13 08:38:52 -0800

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* script that prints 3 lines: (like 1-multi_languages.js)
+  but by using an array of string and a loop */
+const arrLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+arrLang.map(item => console.log(item));
