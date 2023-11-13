3f068742cac90bf5ea68f283c373eaed899ad02d
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-13 07:48:00 -0800

    javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,6 @@
 
 /* script that prints a message depending
   of the number of arguments passed: */
-const { argv } = require('node:process');
+const { argv } = require('process');
 
 if (argv.length === 2) { console.log(' No argument '); } else if (argv.length === 3) { console.log(' Argument found '); } else { console.log(' Arguments found '); }
