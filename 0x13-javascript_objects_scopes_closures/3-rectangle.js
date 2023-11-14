de56f226ee900dfc9a42168979946fa768878829
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-14 05:51:51 -0800

    javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(
+      ('X'.repeat(this.width) + '\n').repeat(this.height).slice(0, -1)
+    );
+  }
+}
+module.exports = Rectangle;
