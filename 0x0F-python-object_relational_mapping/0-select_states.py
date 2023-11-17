da579a851bc8ebd87801846e631decc708ed9dc8
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-17 05:14:16 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,24 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__name__':
-    db = MySQLdb.connect(
-            host='localhost', user=argv[1],
-            port=3306, passwrd=argv[2],
-            db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
 
+    # connect database using the command-line arguments
+    myDB = MySQLdb.connect(
+            host='localhost', user=argv[1],
+            port=3306, passwd=argv[2],
+            db=argv[3])
+    # create the cursor object to iterate with database
+    myCur = myDB.cursor()
+    # exercuting the SELECT quary to fatch the data
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    # fetching all the data returned by the quary
+    rows = cur.fetchall()
+    # iterate the fatch data and print each row
     for row in rows:
         print(row)
+
+    # closing all the cursor
+    myCursor.close()
+
+    # closing all the database
+    myDB.close()
