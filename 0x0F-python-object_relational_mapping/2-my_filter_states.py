f0de65bf2a295bfc27dac8a3d55f4c05555e432b
Ziphozenkosimthombe <123859903+Ziphozenkosimthombe@users.noreply.github.com>
2023-11-18 08:39:06 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,11 +2,10 @@
 """
 script that lists all states from the database hbtn_0e_0_usa:
 """
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-
-    import MySQLdb
-    from sys import argv
 
     # connect database using the command-line arguments
     my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
