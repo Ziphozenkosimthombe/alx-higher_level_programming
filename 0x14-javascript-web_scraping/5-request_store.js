2848ee047314d80f88f7917933b800c6bb9211d3
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-01-04 09:39:29 -0800

    task_05 js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf8', err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
