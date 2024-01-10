b3564b0990f775d8158f5142b261844792b26868
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-01-10 03:24:34 -0800

    task_07

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+$.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    method: 'GET',
+    datatype: 'json',
+    success: function (data) {
+        // extract character from the fetched data
+        const characterName = data.name;
+
+        //display the xcter name in the div
+        $('DIV#character').text(characterName);
+    }
+});
