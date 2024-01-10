ed5a5d556b4557696c1dcc75cc79af3bf0656caf
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-01-10 03:28:29 -0800

    task_09

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,12 @@
+$.ajax({
+      url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        // Extract the translation of "hello" from the fetched data
+        const translation = data.hello;
+
+        // Display the translation in the <div> with ID "hello"
+        $('DIV#hello').text(translation);
+      }
+});
