fb969d34c6ef329d706eb25b20b943a03a56eae1
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-01-10 03:13:24 -0800

    task_03

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,20 @@
+// jQuery script
+$(document).ready(function() {
+
+    // select the div by ID
+    const redHeaderDiv = $('DIV#red_header');
+
+    // add an event handler
+    redHeaderDiv.click(function() {
+        // find the header
+        const headerElement = $('header');
+
+        // check if header was found
+        if (headerElement) {
+            // use addClass method
+            headerElement.addClass('red');
+        } else {
+            console.error('Header elemnet not found');
+        }
+    });
+});
